22e8f1e882d99b819d60a9855086d51a9a58c36b
E <e@pastel.codes>
2021-11-27 15:56:17 +0000

    Add initial files

---
+++ tests/test_board.py
@@ -0,0 +1,11 @@
+import unittest
+from src.board import Board
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        board = Board()
+   
+
+if __name__ == '__main__':
+    unittest.main()
