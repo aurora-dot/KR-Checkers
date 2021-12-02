f505ba0713464efe1ea881c6cf35a7c3c74430cf
E <e@pastel.codes>
2021-12-02 08:56:01 +0000

    Fix starting gui

--- src/checkers/__main__.py
+++ src/checkers/__main__.py
@@ -1,22 +1,4 @@
-import pygame
-from sys import exit
-
-FPS = 60
-WINDOW = pygame.display.set_mode((800, 800))
-
-
-def main() -> None:
-    playing = True
-    clock = pygame.time.Clock()
-
-    while playing:
-        clock.tick(FPS)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
-
+from . import main
 
 if __name__ == "__main__":
     main()
