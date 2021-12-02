0a35b28b0af2b1cda034113517f1c15d66853bc1
E <e@pastel.codes>
2021-12-02 09:04:03 +0000

    Fix GitHub Action with pygame GUI

---
+++ src/checkers/main.py
@@ -0,0 +1,21 @@
+import pygame
+from sys import exit
+
+FPS = 60
+WINDOW = pygame.display.set_mode((800, 800))
+
+
+def main() -> None:
+    playing = True
+    clock = pygame.time.Clock()
+
+    while playing:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+
+main()
