0854234348c43771cffd6b0b4efac20830a52337
E <e@pastel.codes>
2021-12-04 22:41:02 +0000

    Fix traversal

--- src/checkers/human.py
+++ src/checkers/human.py
@@ -25,30 +25,41 @@ class Human(Player):
     def place_piece(self, tile_location):
         row, col = tile_location
         if self.board.board[row][col] == 1:
+            print("Bruh")
+            print(self.selected_piece[0:2], (row + 1, col + 1))
             if (
-                self.board.pieces[row][col]
-                and self.board.pieces[row][col].type == 0
+                self.selected_piece[0:2] == (row + 1, col + 1)
+                or self.selected_piece[0:2] == (row + 1, col - 1)
+                or self.selected_piece[0:2] == (row - 1, col + 1)
+                or self.selected_piece[0:2] == (row - 1, col - 1)
             ):
-                print("bad")
-            if not self.board.pieces[row][col] or (
-                self.board.pieces[row][col]
-                and self.board.pieces[row][col].type != 0
-            ):
-                print("okay-ish")
-                if not self.selected_piece[2].king:
-                    print("Warmer")
-                    if row < self.selected_piece[0]:
-                        if row == 0:
-                            self.selected_piece[2].king = True
-                            print("Made piece king")
 
-                        print("yay! but only forward")
+                if (
+                    self.board.pieces[row][col]
+                    and self.board.pieces[row][col].type == 0
+                ):
+                    print("bad")
+                elif not self.board.pieces[row][col] or (
+                    self.board.pieces[row][col]
+                    and self.board.pieces[row][col].type != 0
+                ):
+                    print("okay-ish")
+                    if not self.selected_piece[2].king:
+                        print("Warmer")
+                        if row < self.selected_piece[0]:
+                            if row == 0:
+                                self.selected_piece[2].king = True
+                                print("Made piece king")
+
+                            print("yay! but only forward")
+                            self.board.move_piece(
+                                self.selected_piece[0:2], (row, col)
+                            )
+                            self.selected_piece = None
+                    else:
                         self.board.move_piece(
                             self.selected_piece[0:2], (row, col)
                         )
                         self.selected_piece = None
-                else:
-                    self.board.move_piece(self.selected_piece[0:2], (row, col))
-                    self.selected_piece = None
-                    print("King moved")
-                    print("yay!")
+                        print("King moved")
+                        print("yay!")
