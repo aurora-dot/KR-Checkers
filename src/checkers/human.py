6ae1ccd1bf46972a33936ec312889c545571969c
E <e@pastel.codes>
2021-12-04 14:31:36 +0000

    Remove old commented out code

--- src/checkers/human.py
+++ src/checkers/human.py
@@ -17,6 +17,7 @@ class Human(Player):
             # check that there are possible valid moves
             #   e.g., all row in line ahead is full
             # display possible valid moves
+
             self.selected_piece = (row, col, self.board.pieces[row][col])
             print(self.selected_piece)
             print("Selected")
@@ -51,57 +52,3 @@ class Human(Player):
                     self.selected_piece = None
                     print("King moved")
                     print("yay!")
-
-    # def place_piece(self, tile_location):
-    #     # Need to do proximity check etc
-    #     row, col = tile_location
-    #     print("hit 2")
-    #     if self.board.board[row][col] == 1:
-    #         if self.board.pieces[row][col]:
-    #             if self.board.pieces[row][col].type == 0:
-    #                 print("bad, not places")
-
-    #             elif self.board.pieces[row][col] != 0:
-    #                 print(row, self.selected_piece[0])
-    #                 if (
-    #                     not self.selected_piece[2].king
-    #                     and row == self.selected_piece[0] - 1
-    #                 ):
-    #                     self.board.move_piece(
-    #                         self.selected_piece[0:2], (row, col)
-    #                     )
-    #                     print("good, placed")
-    #                     if row == 0:
-    #                         self.selected_piece[2].king = True
-    #                         print("Made piece king")
-    #                     self.selected_piece = None
-    #                 else:
-    #                     self.board.move_piece(
-    #                         self.selected_piece[0:2], (row, col)
-    #                     )
-    #                     print("King moved")
-    #                     self.selected_piece = None
-
-    #         # Can only move bottom up as a rule (top down for ai).
-    #         # # TODO Above, fix this cos it broke
-    #         # When reaches the top they will turn into a king
-    #         #   so they can move back and forth.
-
-    #         else:
-    #             print(row, self.selected_piece[0])
-    #             if (
-    #                 not self.selected_piece[2].king
-    #                 and row == self.selected_piece[0] - 1
-    #             ):
-    #                 self.board.move_piece
-    #                   (self.selected_piece[0:2], (row, col))
-    #                 print("rah, placed")
-    #                 if row == 0:
-    #                     self.selected_piece[2].king = True
-    #                     print("Made piece king")
-    #                 self.selected_piece = None
-    #             else:
-    #                 self.board.move_piece
-    #                   (self.selected_piece[0:2], (row, col))
-    #                 self.selected_piece = None
-    #                 print("King moved")
