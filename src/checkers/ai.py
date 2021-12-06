043fb4177cbc031dab71ceb604d02256243825e6
E <e@pastel.codes>
2021-12-06 20:55:18 +0000

    Add difficulty attribute to ai class

--- src/checkers/ai.py
+++ src/checkers/ai.py
@@ -4,6 +4,8 @@ from .player import Player
 class Ai(Player):
     def __init__(self, type, board, game) -> None:
         super().__init__(type, board, game)
+        # Should be three levels of difficulty, 1, 2, 3
+        self.level = 1
 
     def select_piece(self, tile_location):
         pass
