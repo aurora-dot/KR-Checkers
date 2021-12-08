bf67d90970c6d24d9a08b9c9cbdad6644c33406c
E <e@pastel.codes>
2021-12-08 12:07:26 +0000

    just saving

--- src/checkers/game.py
+++ src/checkers/game.py
@@ -59,29 +59,28 @@ class Game:
         ]
 
     def calculate_heuristic(self, board: Board):
-        scores = [0, 0]
+        score = 0
+        pieces_scores = [0, 0]
 
         for row in range(8):
             for col in range(8):
                 piece = board.pieces[row][col]
 
                 if piece:
-                    # Pieces
-                    scores[piece.type] += 5
+                    pieces_scores[piece.type] += 200
 
-                    # King
                     if piece.king:
-                        scores[piece.type] += 300
-                        scores[1 if piece.type == 0 else 0] -= 500
+                        if piece.type == 0:
+                            score -= 500
+                        else:
+                            score += 300
 
-                    # Backrow
-                    if (piece.type == 0 and row == 7) or (
-                        piece.type == 1 and row == 0
-                    ):
-                        scores[piece.type] += 50
+                    if piece.type == 1 and row == 0:
+                        score += 75
 
-        human_score, ai_score = scores
-        return ai_score - human_score
+        score += pieces_scores[0] - pieces_scores[1]
+
+        return score
 
     def validate_move(self, piece, piece_location, tile_location, board):
         og_row, og_col = piece_location
